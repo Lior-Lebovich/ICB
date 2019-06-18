ef9535c3fb3a9d0f8bedc144549cc6622cabf970
Lior-Lebovich <51818980+Lior-Lebovich@users.noreply.github.com>
2019-06-18 04:14:55 +0300

    Add files via upload

---
+++ hddm related code and data/g3/informative priors 40K/drift/g3_drift.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+job='job_'
+output='output_'
+dat='g3_drift_'
+numrun=0
+qsub -N "$job$dat$numrun" -o "$output$dat$numrun" -b y "source ~/envs/yon/bin/activate; python g3_drift.py "$numrun""
+numrun=1
+qsub -N "$job$dat$numrun" -o "$output$dat$numrun" -b y "source ~/envs/yon/bin/activate; python g3_drift.py "$numrun""
+numrun=2
+qsub -N "$job$dat$numrun" -o "$output$dat$numrun" -b y "source ~/envs/yon/bin/activate; python g3_drift.py "$numrun""
+numrun=3
+qsub -N "$job$dat$numrun" -o "$output$dat$numrun" -b y "source ~/envs/yon/bin/activate; python g3_drift.py "$numrun""
+numrun=4
+qsub -N "$job$dat$numrun" -o "$output$dat$numrun" -b y "source ~/envs/yon/bin/activate; python g3_drift.py "$numrun""
+numrun=5
+qsub -N "$job$dat$numrun" -o "$output$dat$numrun" -b y "source ~/envs/yon/bin/activate; python g3_drift.py "$numrun""
+numrun=6
+qsub -N "$job$dat$numrun" -o "$output$dat$numrun" -b y "source ~/envs/yon/bin/activate; python g3_drift.py "$numrun""
+numrun=7
+qsub -N "$job$dat$numrun" -o "$output$dat$numrun" -b y "source ~/envs/yon/bin/activate; python g3_drift.py "$numrun""
+numrun=8
+qsub -N "$job$dat$numrun" -o "$output$dat$numrun" -b y "source ~/envs/yon/bin/activate; python g3_drift.py "$numrun""
+numrun=9
+qsub -N "$job$dat$numrun" -o "$output$dat$numrun" -b y "source ~/envs/yon/bin/activate; python g3_drift.py "$numrun""
+numrun=10
+qsub -N "$job$dat$numrun" -o "$output$dat$numrun" -b y "source ~/envs/yon/bin/activate; python g3_drift.py "$numrun""
+numrun=11
+qsub -N "$job$dat$numrun" -o "$output$dat$numrun" -b y "source ~/envs/yon/bin/activate; python g3_drift.py "$numrun""
