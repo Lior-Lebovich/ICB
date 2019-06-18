819f442cd1279627ca5ca27ad13efa2566454f1f
Lior-Lebovich <51818980+Lior-Lebovich@users.noreply.github.com>
2019-06-18 05:58:48 +0300

    Add files via upload

---
+++ hddm related code and data/motor1/informative priors 40K/none/motor_none1.py
@@ -0,0 +1,39 @@
+
+import pandas as pd
+import os
+import csv
+import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+import kabuki
+from kabuki.analyze import gelman_rubin
+import hddm
+print(hddm.__version__)
+# Python version:   
+import sys
+print (sys.version)
+print (pd.__version__)
+
+# Adding a monkey in order to save the models dbs:
+import pickle
+# Get around a problem with saving regression outputs in Python 3
+def savePatch(self, fname):
+    with open(fname, 'wb') as f:
+        pickle.dump(self, f)
+hddm.HDDM.savePatch = savePatch
+
+
+data = hddm.load_csv("/ems/elsc-labs/loewenstein-y/lior.lebovich/DD/motor1/YONI_1.csv")
+data.rt = data.rt - 0.75
+data = data[data.rt<=3]
+docdir = "/ems/elsc-labs/loewenstein-y/lior.lebovich/DD/motor1/none1"
+os.chdir(docdir)
+
+i = sys.argv[1]
+print(i)
+m = hddm.HDDM(data, 
+			  p_outlier=.05, informative=True)
+m.find_starting_values()
+m.sample(40000, burn=20000, thin=5, dbname='db'+str(i), db='pickle')
+m.savePatch('submodel'+str(i))
+
+print('Thats it!!')
