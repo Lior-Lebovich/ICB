df272119de6e48d12f0c6512b8b4e417e99f28ba
Lior-Lebovich <51818980+Lior-Lebovich@users.noreply.github.com>
2019-06-18 06:30:21 +0300

    Add files via upload

---
+++ hddm related code and data/motor6/informative priors 40K/drift/motor_drift6.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+job='job_'
+output='output_'
+dat='motor_drift6_'
+numrun=0
+qsub -N "$job$dat$numrun" -o "$output$dat$numrun" -b y "source ~/envs/yon/bin/activate; python motor_drift6.py "$numrun""
+numrun=1
+qsub -N "$job$dat$numrun" -o "$output$dat$numrun" -b y "source ~/envs/yon/bin/activate; python motor_drift6.py "$numrun""
+numrun=2
+qsub -N "$job$dat$numrun" -o "$output$dat$numrun" -b y "source ~/envs/yon/bin/activate; python motor_drift6.py "$numrun""
+numrun=3
+qsub -N "$job$dat$numrun" -o "$output$dat$numrun" -b y "source ~/envs/yon/bin/activate; python motor_drift6.py "$numrun""
+numrun=4
+qsub -N "$job$dat$numrun" -o "$output$dat$numrun" -b y "source ~/envs/yon/bin/activate; python motor_drift6.py "$numrun""
+numrun=5
+qsub -N "$job$dat$numrun" -o "$output$dat$numrun" -b y "source ~/envs/yon/bin/activate; python motor_drift6.py "$numrun""
+numrun=6
+qsub -N "$job$dat$numrun" -o "$output$dat$numrun" -b y "source ~/envs/yon/bin/activate; python motor_drift6.py "$numrun""
+numrun=7
+qsub -N "$job$dat$numrun" -o "$output$dat$numrun" -b y "source ~/envs/yon/bin/activate; python motor_drift6.py "$numrun""
+numrun=8
+qsub -N "$job$dat$numrun" -o "$output$dat$numrun" -b y "source ~/envs/yon/bin/activate; python motor_drift6.py "$numrun""
+numrun=9
+qsub -N "$job$dat$numrun" -o "$output$dat$numrun" -b y "source ~/envs/yon/bin/activate; python motor_drift6.py "$numrun""
+numrun=10
+qsub -N "$job$dat$numrun" -o "$output$dat$numrun" -b y "source ~/envs/yon/bin/activate; python motor_drift6.py "$numrun""
+numrun=11
+qsub -N "$job$dat$numrun" -o "$output$dat$numrun" -b y "source ~/envs/yon/bin/activate; python motor_drift6.py "$numrun""
